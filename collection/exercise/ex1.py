1c21fbc8a3ba573ea079c365b7b0849972552af0
Amir Ammar <AmirAmmr123@github.com>
2023-09-18 05:35:38 +0300

    ex1.py in collection/exercise was modified

--- collection/exercise/ex1.py
+++ collection/exercise/ex1.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 
+"""
+Exercise 1
+
+Given the following two lists:
+
 first = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 second = ["day", "day", "sday", "nesday", "rsday", "day", "urday"]
-[ print("".join(x)) for x in zip(first ,second) ]
+
+    Concatenate the two lists by index into a new list that, when printed, looks as follows:
+
+["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+"""
+
+
+
+first = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+second = ["day", "day", "sday", "nesday", "rsday", "day", "urday"]
+print(list("".join(x) for x in zip(first ,second)))
